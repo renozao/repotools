2e52eab678daba9ae6ca73ece00bdc84ab9f00da
Renaud <renaud@tx.technion.ac.il>
2014-06-19 10:19:24 +0200

    GitHub webhook: now checks for src/ sub-directory so that the field NeedsCompilation is correct

--- inst/www/index.php
+++ inst/www/index.php
@@ -8,6 +8,32 @@ function die_result($msg){
 	die();
 }
 
+function flag_update(){
+	touch("src/do-update");
+}
+
+function api_get_contents($user, $repo, $ref, $path){
+	$url = "https://api.github.com/repos/$user/$repo/contents/$path?ref=$ref";
+	
+	// create curl resource
+    $ch = curl_init();
+
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+    // $output contains the output string
+    $output = curl_exec($ch);
+
+    // close curl resource to free up system resources
+    curl_close($ch); 
+    
+	return json_decode($output);
+}
+
 // load local config
 // contains: $secret = 'YOUR_OWN_HOOK_SECRET';
 include("config.php");
@@ -32,6 +58,7 @@ if( isset($_POST['payload']) ){
 
 	//print_r($data);
 	$repo_name = $data->repository->name;
+	$user = $data->repository->owner->name;
 	echo "* Received pushed notification from repository '".$repo_name."'\n";
 	// extract relevant data
 	echo "* Checking pushed branch name ... ";
@@ -66,14 +93,36 @@ if( isset($_POST['payload']) ){
 		// create package src/contrib directory if necessary
 		if( !is_dir($pkg_dir = dirname($DESCRIPTION_file)) ) mkdir($pkg_dir, 0777, true);
 		file_put_contents($DESCRIPTION_file, $desc);
-		file_put_contents($DESCRIPTION_file, "GHuser: ".$data->repository->owner->name."\n", FILE_APPEND);
+		file_put_contents($DESCRIPTION_file, "GHuser: ".$user."\n", FILE_APPEND);
 		file_put_contents($DESCRIPTION_file, "GHref: ".$ref."\n", FILE_APPEND);
 		file_put_contents($DESCRIPTION_file, "GHfork: ".($data->repository->fork ? 'yes' : 'no')."\n", FILE_APPEND);
 		echo "[OK]\n";
 		
 		// flag for update
-		touch("src/do-update");
+		flag_update();
 	}else echo "[SKIP: no changes]\n";
+	
+	echo "* Checking src/ sub-directory ... ";
+	// look for src/ sub-directory
+	$local_src = "{$GRAN_contrib}{$repo_name}-{$suffix}/src";
+	if( count(api_get_contents($user, $repo_name, $ref, 'src')) ){
+		if( is_dir($local_src) ) echo "[SKIP: no changes]\n";
+		else{
+		
+			if( mkdir($local_src) ){
+				echo "[CREATED]\n";
+				// flag for update
+				flag_update();
+			}else echo "[ERROR: failed creating src/]\n";
+		}
+	}else if( is_dir($local_src) ){
+		if( rmdir($local_src) ){
+			echo "[DELETED]\n";
+			// flag for update
+			flag_update();
+		}else echo "[ERROR: failed deleting src/]\n";
+	}else echo "[SKIP: no changes]\n";
+	
 }
 
 /* cron job does:
