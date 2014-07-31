cffefbcf62da166a05f0e7873ce2295f3ce3876c
Renaud <renaud@tx.technion.ac.il>
2014-07-31 16:36:06 +0200

    Enable setting credentials (cache or disk)

--- R/url.R
+++ R/url.R
@@ -9,7 +9,7 @@
 #' Parses an netrc file in different ways and return the credential specifications it contains.
 #' 
 #' Entries in the netrc file can be associated to R repositories, which are looked up 
-#' by \code{install.pkgs} via \code{\link{repos.url}}, by adding a comment line 
+#' by \code{install.pkgs} via \code{\link{repos_url}}, by adding a comment line 
 #' to a standard entry the following format:
 #' 
 #' \preformatted{
@@ -38,6 +38,8 @@
 #' @param std indicates if the file should be parsed as a standard netrc file (\code{TRUE}), 
 #' searched for R repository specifications (see \emph{Details}) (\code{FALSE}), 
 #' or if entries associated with R repositories should be skipped (\code{NA}).
+#' @param line.number logical that indicates if the starting and ending line number of each 
+#' credential definition should also be returned.
 #' @param quiet logical that indicates if the function should return \code{NULL} if the file is not found, 
 #' or throw an error.
 #' 
@@ -59,21 +61,25 @@
 #' 
 #' # standard parsing
 #' read_netrc(f)
+#' read_netrc(f, line.number = TRUE)
 #' # parse repo information
 #' read_netrc(f, std = FALSE)
 #' # skip repo entries
 #' read_netrc(f, std = NA)
 #'  
 #' 
-read_netrc <- function(x = file.path(Sys.getenv('HOME'), '.netrc'), std = TRUE, quiet = FALSE){
+read_netrc <- function(x = .netrc_path(), std = TRUE, line.number = FALSE, quiet = FALSE){
     
-    if( is.null(x) ) x <- file.path(Sys.getenv('HOME'), '.netrc')
-    if( is.character(x) && !file.exists(x) ){
-        if( !quiet ) stop("netrc file '", x, "' does not exist.")
-        return()
-    }
+    x <- x %||% .netrc_path()
+    if( is.character(x) ){
+        if( !file.exists(x) ){
+            if( !quiet ) stop("netrc file '", x, "' does not exist.")
+            return()
+        }
+        netrc_file <- normalizePath(x)
+    }else netrc_file <- x
     
-    l <- readLines(x, warn = FALSE)
+    l <- readLines(netrc_file, warn = FALSE)
     if( !length(l) ) return()
     
     chunk <- grep("^\\s*$", l)
@@ -104,20 +110,43 @@ read_netrc <- function(x = file.path(Sys.getenv('HOME'), '.netrc'), std = TRUE, 
             # add trailing path if not standard parsing
             if( isFALSE(std) ){
                 if( is_NA(def['repos']) ){
-                    def['repos'] <- def['machine']
+                    def['repos'] <- NA #def['machine']
                     def['path'] <- ''
                 }else{
                     p <- str_match(unname(def['repos']), '^@?([^ /]+)(/[^ ]+)?')
-                    def['repos'] <- paste0('@', p[, 2L])
+#                    def['repos'] <- paste0('@', p[, 2L])
+                    def['repos'] <- p[, 2L]
                     def['path'] <- p[, 3L]    
                 }
-                
             }else def <- def[std_fields]
+            
+            # add start/end index
+            if( line.number ) def[c('start', 'end')] <- c(chunk[i-1L]+1L, chunk[i]-1L)
             
             def
         })
     res <- Filter(length, res)
-    do.call(rbind, res)
+    res <- do.call(rbind, res)
+    rownames(res) <- res[, 'machine']
+    # attach file name
+    attr(res, '.netrc') <- netrc_file
+    
+    res
+}
+
+#' @export
+.netrc_path <- function(){
+    file.path(Sys.getenv('HOME'), '.netrc')
+}
+
+url_credential_split <- function(x){
+    p <- "^(((https?)|(file))://)?(([^:/]*):([^@:/]*))?((@)?([^/]*))(.*)"
+    url <- str_match(x, p)
+    res <- cbind(machine = url[, 11L], login = url[, 7L], password = url[, 8L]
+                , repos = ifelse(!grepl(".", url[, 11L], fixed = TRUE), url[, 11L], NA)
+                , path = url[, 12L])
+    rownames(res) <- names(x)
+    res 
 }
 
 #' Repository URLs
@@ -145,8 +174,8 @@ read_netrc <- function(x = file.path(Sys.getenv('HOME'), '.netrc'), std = TRUE, 
 #' @examples
 #' 
 #' # appending to default repos
-#' repos.url('+')
-#' repos.url('+http://another.repo.org')
+#' repos_url('+')
+#' repos_url('+http://another.repo.org')
 #' 
 #' # using toy example
 #' f <- system.file('netrc', package = 'repotools')
@@ -154,9 +183,9 @@ read_netrc <- function(x = file.path(Sys.getenv('HOME'), '.netrc'), std = TRUE, 
 #' 
 #' # test different substitutions
 #' spec <- c('@@myRepo', '@@myRepo/extra/path', 'http://www.public.org', 'http://www.privatehost.org/repo/dir', '@@CRAN@@')
-#' cbind(Specification = spec, URL = repos.url(spec, .netrc = f))
+#' cbind(Specification = spec, URL = repos_url(spec, .netrc = f))
 #' 
-repos.url <- function(repos = getOption('repos'), ..., .netrc = NULL){
+repos_url <- function(repos = getOption('repos'), ..., .netrc = NULL){
     
     x <- c(repos, unlist(list(...), use.names = FALSE))
     
@@ -167,22 +196,33 @@ repos.url <- function(repos = getOption('repos'), ..., .netrc = NULL){
         x <- c(getOption('repos'), x)
     }
     
-    if( length(i <- which(x == '@CRAN@')) ){
+    # CRAN
+    if( length(i <- which(toupper(x) == '@CRAN@')) ){
         if( !interactive() ) x[i] <- 'http://cran.rstudio.com' 
         else if( !is.na(cran_url <- getOption('repos')['CRAN']) ) x[i] <- cran_url
+    }
+    # Bioconductor
+    if( length(i <- which(toupper(x) == '@BIOC')) ){
+        x <- x[-i]
+        x <- .biocinstallRepos(x)
+    }
+    # Omegahat
+    if( length(i <- which(toupper(x) == '@OMEGAHAT')) ){
+        x[i] <- 'http://www.omegahat.org/R'
     }
     
     # load credentials and repo keys from netrc file
     if( !is.null(net <- read_netrc(x = .netrc, std = FALSE, quiet = TRUE)) ){
         # complete repo or add credentials
-        url <- str_match(x, "^(((http)|(file))://)?((@)?([^/]+))(.*)")
+        #url <- str_match(x, "^(((http)|(file))://)?((@)?([^/]+))(.*)")
+        url <- url_credential_split(x)
         j <- rep(0L, nrow(url))
-        rkeys <- nzchar(url[, 7L])
-        j[rkeys] <- match(url[rkeys, 6L], net[, 'repos'], nomatch = 0L)
-        j[!rkeys] <- match(url[!rkeys, 6L], net[, 'machine'], nomatch = 0L)
+        rkeys <- !is.na(url[, 'repos'])
+        j[rkeys] <- match(url[rkeys, 'repos'], net[, 'repos'], nomatch = 0L)
+        j[!rkeys] <- match(url[!rkeys, 'machine'], net[, 'machine'], nomatch = 0L)
         url <- url[j>0, , drop = FALSE]
         net <- net[j, , drop = FALSE]
-        x[j>0] <- sprintf("http://%s:%s@%s%s%s", net[, 'login'], net[, 'password'], net[, 'machine'], net[, 'path'], url[, 9L])       
+        x[j>0] <- sprintf("http://%s:%s@%s%s%s", net[, 'login'], net[, 'password'], net[, 'machine'], net[, 'path'], url[, 'path'])       
     }
     
     if( length(i <- setdiff(grep("^@", x), which(x == '@CRAN@'))) ){
@@ -193,3 +233,183 @@ repos.url <- function(repos = getOption('repos'), ..., .netrc = NULL){
     x
 }
 
+override_matrix <- function(x, y){
+    if( !length(x) ) y
+    else{
+        im <- match(rownames(y), rownames(x), nomatch = 0L)
+        x[im[im>0], ] <- y[im>0, , drop = FALSE]
+        rbind(x, y[im==0L, , drop = FALSE])
+    }
+} 
+
+#' CRAN-style Repository Authentication Credentials
+#' 
+#' Gets/sets authentication credentials for CRAN-style.
+#' 
+#' @param ... repository keys or credential specifications
+#' @param save indicates if the new credentials are to be saved on disk,
+#' by updating in the \code{.netrc} file, or in a cache -- which expires at the end of 
+#' the \pkg{R} session.
+#' It can also be a character string, which then specifies the path where to save the 
+#' result (cached entries are not written to disk).
+#' 
+#' Use \code{save=""} to print to the console.
+#'  
+#' @param cache specifies how the temporary cached credentials should be 
+#' loaded: 
+#' 
+#' \itemize{
+#' \item \code{NULL} loads both the cache and netrc data, with cache override matching 
+#' netrc credentials;
+#' \item \code{TRUE} loads only the cache data; 
+#' \item \code{FALSE} loads only the netrc data.
+#' }
+#' 
+#' The special value \code{cache = NA} will delete the cache, prior to any other operation.
+#' 
+#' @param exact logical that indicates if the the repository keys should be matched 
+#' exactly. If \code{FALSE}, then keys are matched independently of possible trailing 
+#' path specification.
+#' @inheritParams repos_url
+#' 
+#' @export
+repos_auth <- local({
+    
+    .auth_cache <- NULL
+    function(..., save = FALSE, cache = NULL, exact = FALSE, .netrc = NULL){
+        
+        x <- list(...)
+        
+        if( !is.matrix(.netrc) ){
+            net <- read_netrc(x = .netrc, std = FALSE, line.number = TRUE, quiet = TRUE) 
+        }else net <- .netrc
+        
+        # pre-process netrc data
+        netrc_file <- attr(net, '.netrc')
+        
+        if( !is.null(net) ){
+            net <- net[!is.na(net[, 'repos']), , drop = FALSE]
+        }
+        # extract credential data 
+        auth <- net[, !colnames(net) %in% c('start', 'end'), drop = FALSE]
+        rownames(auth) <- auth[, 'repos']
+        
+        # append/override cache
+        if( is_NA(cache) ){
+            old <- .auth_cache
+            .auth_cache <<- NULL
+            cache <- NULL
+            
+            # return old cache if nothing else to do
+            if( !length(x) ) return(invisible(old))
+        }
+        
+        if( isTRUE(cache) ) auth <- .auth_cache
+        else if( !isFALSE(cache) && !is.null(.auth_cache) && nrow(.auth_cache) ){
+            auth <- override_matrix(auth, .auth_cache)
+        }
+        
+        # list of auths
+        if( !length(x) ) return(auth)
+        
+        if( length(x) == 1L && is.null(names(x)) ) x <- x[[1L]]
+        
+        # build empty matrix if necessary
+        if( !length(auth) ){
+            fields <- c('machine', 'login', 'password', 'repos', 'path')
+            auth <- matrix(NA, nrow=0, ncol=length(fields), dimnames = list(NULL, fields))
+        }
+        
+        if( !is.matrix(x) ){
+            # read access
+            if( is.null(names(x)) ){
+                
+                if( exact ) ia <- match(x, auth[, 'machine'])
+                else{
+                    ix <- pmatch(auth[, 'machine'], x)
+                    ia <- match(seq_along(x), ix)
+                }
+                # force the repos query
+                irepo <- grep("^((@.+)|([^.]+))$", x)
+                ia[irepo] <- match(gsub("^@", '', x[irepo]), auth[, 'repos'])
+                
+                res <- auth[ia, , drop = FALSE]
+                if( is.matrix(res) ) rownames(res) <- x 
+                
+                return(res)
+            }
+            
+            # split into a matrix
+            if( is.list(x) || is.character(x) ){
+                x[sapply(x, length) == 0L] <- NA
+                x <- unlist(x)
+                ok <- !is.na(x)
+                x <- url_credential_split(x)
+                # re-format
+                x[ok, 'machine'] <- paste0(x[ok, 'machine'], x[ok, 'path'])     
+                x[, 'path'] <- gsub("^[^/]+", '', rownames(x))
+                x[, 'repos'] <- gsub("^([^/]+).*", '\\1', rownames(x))
+                rownames(x) <- x[, 'repos'] 
+            }
+        }
+        
+        
+        if( !is.matrix(x) ) 
+            stop("Unexpected error: invalid processed input [", class(x), "]")
+        
+        new_auth <- x
+        old_auth <- repos_auth(unname(rownames(new_auth)), .netrc = net)
+        
+        # force no credentials
+        if( length(i_nocred <- which(!nzchar(new_auth[, 'machine']))) ){
+            new_auth[i_nocred, c('machine', 'path')] <- old_auth[i_nocred, c('machine', 'path')]
+        } 
+        
+        # store in cache
+        if( isFALSE(save) ){
+            # remove NA flagged rows
+            if( is.null(.auth_cache) ) .auth_cache <<- new_auth
+            else .auth_cache <<- override_matrix(.auth_cache, new_auth)
+            .auth_cache <<- .auth_cache[!is.na(.auth_cache[, 'machine']), , drop = FALSE]
+            
+            return(invisible(old_auth))
+        }
+        
+        flag_delete <- digest(tempfile())
+        fields <- c('machine', 'login', 'password')
+        l <- readLines(netrc_file) 
+        sapply(rownames(new_auth), function(r){
+            
+            # new entry
+            new_data <- new_auth[r, ]
+            to_delete <- is.na(new_data['machine'])
+            new_entry <- sprintf("#repos @%s%s\n%s", new_data['repos'], new_data['path'], paste0(sprintf("%s %s", fields, new_data[fields]), collapse = "\n"))
+            
+            if( length(net) && (i <- match(r, net[, 'repos'], nomatch = 0L)) ){
+                idx <- seq(as.integer(net[i,'start']), as.integer(net[i,'end']))
+                if( to_delete ){ # delete
+                    l[idx] <<- flag_delete
+                    if( !nzchar(l[idx[1L] - 1L]) ) l[idx[1L] - 1L] <<- flag_delete
+                }else{ # modify
+                    l[idx] <<- flag_delete
+                    l[idx[1L]] <<- new_entry
+                }
+                
+            }else if( !to_delete ){ # add
+                if( nzchar(tail(l, 1L)) ) new_entry <- c("", new_entry) 
+                l <<- c(l, new_entry)
+            }
+        })
+        
+        # remove flagged lines
+        l <- l[l != flag_delete]
+        
+        # update file
+        if( is.character(save) ) netrc_file <- save
+        cat(l, file = netrc_file, sep = "\n")
+        message(sprintf("Updated %s repos credentials in %s", nrow(new_auth), netrc_file))
+    
+        # return old value
+        invisible(old_auth)
+    }
+})
--- R/utils.R
+++ R/utils.R
@@ -20,25 +20,25 @@ smessage <- function(..., indent = 0L, item = NULL, appendLF = FALSE){
     message(indent, item, ..., appendLF = appendLF)
 }
 
-# set package options
-.opts <- setupPackageOptions(auth = NULL)
-           
-#' Package Options for repotools
-#'
-#' These functions behave like the base functions \code{\link{options}}
-#' and \code{\link{getOption}}, and provide access to options that are specific
-#' to the \pkg{repotools} package. 
-#' 
-#' @inheritParams base::options
-#' @inheritParams base::getOption
-#'           
-#' @rdname options
-#' @export 
-repos.getOption <- .opts$getOption
-
-#' @rdname options
-#' @export 
-repos.options <- .opts$options
+## # set package options
+## .opts <- setupPackageOptions(auth = NULL)
+##            
+## #' Package Options for repotools
+## #'
+## #' These functions behave like the base functions \code{\link{options}}
+## #' and \code{\link{getOption}}, and provide access to options that are specific
+## #' to the \pkg{repotools} package. 
+## #' 
+## #' @inheritParams base::options
+## #' @inheritParams base::getOption
+## #'           
+## #' @rdname options
+## #' @export 
+## repos.getOption <- .opts$getOption
+## 
+## #' @rdname options
+## #' @export 
+## repos.options <- .opts$options
 
 # From example in ?toupper
 capwords <- function(s, strict = FALSE) {
