e0ed6ab1b4dbe329181995e259426a25a85a84aa
Renaud <renaud@tx.technion.ac.il>
2017-03-15 13:36:23 +0200

    Bumped installer to 1.8.9

--- install.R
+++ install.R
@@ -52,7 +52,7 @@ if( !require_version('devtools', version = '1.12') ){
 }
 
 # load/install repotools
-if( !require_version('repotools', version = '1.8.8') ){
+if( !require_version('repotools', version = '1.8.9') ){
   # install repotools
   install_github('renozao/repotools', quick = QUICK)
   library(repotools)
