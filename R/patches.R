5804a4a0e9541134b8e1e3a0376cf1501e726d48
Renaud <renaud@tx.technion.ac.il>
2017-05-16 10:03:05 +0200

    Added function get_shim_parent to retrieve a shim original function

--- R/patches.R
+++ R/patches.R
@@ -5,26 +5,22 @@
 
 .shim_registry <- new.env()
 
-set_shim <- function(envir, FUN, name = NULL, quiet = FALSE){
+set_shim <- function(name, FUN, envir = NULL, quiet = FALSE){
   
   # process envir
-  if( isString(envir) ){
-    envir <- strsplit(envir, '::')[[1L]]
-    name <- name %||% envir[2L]
-    envir <- asNamespace(envir[1L])
-  }
-  if( is.null(name) || !nzchar(name) ) stop("Invalid target function name (empty)")
-  
-  ename <- packageName(envir)
-  # force function's environment
-  fpkg <- packageName(topenv(environment(FUN)))
-  key <- paste0(ename, '::', name)
+  spec <- parse_function_name(name, envir = envir)
+  ename <- spec$ename
+  name <- spec$name
+  key <- spec$key
+  envir <- spec$envir
   
   # backup original definition if necessary
   if( is.null(.shim_registry[[key]]) ){
     .shim_registry[[key]] <- envir[[name]]
   }
   
+  # force function's environment
+  fpkg <- packageName(topenv(environment(FUN)))
   eFUN <- FUN
   environment(eFUN) <- environment(envir[[name]])
   
@@ -56,6 +52,37 @@ set_shim <- function(envir, FUN, name = NULL, quiet = FALSE){
   
 }
 
+
+parse_function_name <- function(name, envir = NULL){
+  
+  # check for format ns::name
+  parts <- strsplit(name, '::')[[1L]]
+  if( length(parts) > 1L ){
+    envir <- envir %||% parts[[1L]]
+    name <- parts[2L]
+  }
+  envir <- asNamespace(envir)
+  ename <- packageName(envir)
+  if( is.null(name) || !nzchar(name) ) stop("Invalid target function name (empty)")
+  
+  # return environment, name and key (explicit name)
+  list(envir = envir
+      , ename = ename
+      , name = name
+      , key = paste0(ename, '::', name)
+  )
+  
+}
+
+# returns the original function that has been patched
+get_shim_parent <- function(name, ...){
+  # extract key from name
+  spec <- parse_function_name(name, ...)
+  # return stored original function
+  .shim_registry[[spec$key]]
+  
+}
+
 set_shims <- function(ns = topenv(parent.frame()), quiet = NULL){
   
   # inject shims into their respective namespace
@@ -83,7 +110,7 @@ set_shims <- function(ns = topenv(parent.frame()), quiet = NULL){
   
 }
 
-reset_shim <- function(envir, name = NULL){
+reset_shim <- function(name, envir = NULL){
   
   # process envir
   if( isString(envir) ){
@@ -104,10 +131,12 @@ reset_shim <- function(envir, name = NULL){
   
 }
 
-with_shim <- function(envir, FUN, expr, name = NULL){
+with_shim <- function(name, FUN, expr, envir = NULL){
   
   # restore shim on exit
-  on.exit(reset_shim(envir, name))
-  if( !set_shim(envir, name) ) on.exit()
+  on.exit(reset_shim(name, envir))
+  if( !set_shim(name, FUN, envir) ) on.exit()
+  e <- parent.frame()
+  eval(expr, envir = e)
   
 }
